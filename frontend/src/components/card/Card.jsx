a8c8eb7e8dbfb845cbd34e3739f5685bb6db9684
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2023-10-26 17:01:55 -0300

    -

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 const Card = ({ item }) => {
   return (
     <div className="w-64 pt-4 px-4 pb-2.5 bg-sky-light text-dark rounded-xl flex flex-col items-center text-center">
+      {/* CÓDIGO PARA CUANDO ESTÉ LEVANTADO EL BACK */}
+      {/* <img src={item.image_urls[0]} alt="" />
+      <h2>{item.title}</h2>
+      <h3>{item.genre}</h3> */}
       <img className="w-64 h-72" src={item.images[0].url_image} alt="" />
       <h2 className="my-2">{item.name}</h2>
       <Link to={`/detail/${item.id}`}>
