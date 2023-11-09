b972a60977b573d96f0bc374936fbe472ab4aaa2
Matias Airaldo <matiasairaldoferrari@gmail.com>
2023-11-09 14:56:37 -0300

    Response json and change endpoint to login

--- backend/script.py
+++ backend/script.py
@@ -3,7 +3,7 @@ import json
 
 BASE_URL = "https://api.themoviedb.org/3"
 HEADERS = {
-    'Authorization': 'Bearer API_TOKEN',
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzdlMTkwY2U1N2ZhMGJlNTIwOGMwMmFjODgyNWY1MSIsInN1YiI6IjY1MzlmODg5MDkxZTYyMDBmZWM2M2Q4ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IDaHIxHdJkemN3HtwLbe3Jxejd4ypZk_t0eY-_oRkmc',
     'accept': 'application/json'
 }
 print("1")
@@ -20,9 +20,14 @@ def get_movie_images(movie_id):
         return response.json().get('backdrops', [])[:5]
     else:
         return []
+    
+POST_HEADERS = {
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IlJPTEVfUk9PVCIsIm5hbWUiOiJ1bmRlZmluZWQiLCJsYXN0X25hbWUiOiJ1bmRlZmluZWQiLCJzdWIiOiJ1bmRlZmluZWRAZ21haWwuY29tIiwiaWF0IjoxNjk5NDk2NDY0LCJleHAiOjE2OTk1MzI0NjR9.IfHKBbHicMnSx17AqKZPuEJI6NwMLE5f8hdTPzzid4U',
+    'Content-Type': 'application/json'
+}
 print("3")
 def main():
-    for page in range(1, 10):
+    for page in range(36, 60):
         response = requests.get(f"{BASE_URL}/trending/movie/week?language=es-ES&page={page}", headers=HEADERS)
         print("4")
         if response.status_code == 200:
@@ -40,7 +45,7 @@ def main():
                         "image_urls": image_urls
                     }
                     
-                    response = requests.post('http://localhost:8080/api/movies', headers={'Content-Type': 'application/json'}, json=data_to_send)
+                    response = requests.post('http://18.118.7.75/api/movies', headers=POST_HEADERS, json=data_to_send)
                     print(f"Data for movie {movie['title']} sent. Response status: {response.status_code}")
 
 if __name__ == "__main__":
