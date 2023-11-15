df45a4d06b113b596a85209e800dc662a4d08295
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2023-11-15 18:58:32 -0300

    Probando nueva config de cors.

--- backend/src/main/java/proyecto_final_equipo3/backend/config/CorsConfiguration.java
+++ backend/src/main/java/proyecto_final_equipo3/backend/config/CorsConfiguration.java
@@ -20,6 +20,11 @@ public class CorsConfiguration {
                         .allowedOrigins(ALLOWED_ORIGINS)
                         .allowedMethods(ALLOWED_METHODS)
                         .allowedHeaders(ALLOWED_HEADERS);
+
+                registry.addMapping("/auth/promoteToAdmin/{email}")
+                        .allowedOrigins(ALLOWED_ORIGINS)
+                        .allowedMethods(ALLOWED_METHODS)
+                        .allowedHeaders(ALLOWED_HEADERS);
             }
         };
     }
